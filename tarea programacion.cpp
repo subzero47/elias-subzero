523d36cf6c026fa49eca8574859d28a4ed120a89
subzero47 <35123892+subzero47@users.noreply.github.com>
2018-01-04 22:53:00 -0600

    Add files via upload

---
+++ tarea programacion.cpp
@@ -0,0 +1,41 @@
+cout<<"ingrese su sueldo"<<s<<end1;
+cout<<"el sueldo que ingresaste fue"<<st<<endl;
+      
+      if("si sueldo es s=2000 y s=10000 ")Then; 
+       printf("su sueldo es correcto");
+       
+       else("si su sueldo es menos que 2000");
+        printf("el sueldo que ingresaste es incorrecto repita otra ves");
+        endl;
+        
+      cout<<"ingrese su tipo de pago \n 1.-semanal \n 2.-quincenal \n 3.-mensual" <<endl;
+      cin>>opc;
+    
+    System("sueldo");
+    do{
+    	system("sueldo");
+    	printf("n\pago semanal"); scanf ("%s", semanal);
+    	printf("n\pago quincenal"); scanf ("%s",quincenal);
+    	printf("n\pago mensual "); scanf ("%s", mensual );
+    	
+	}
+
+ switch (opc) // el parentesis es para la variable que se va a comprobar..//
+ {
+ 	case 1: // haz seleccionado semanal //
+ 	printf("sueldo es igual a /7");
+ 	break;
+ 	case 2: // haz seleccionado quincenal//
+ 	printf("sueldo es igual a /15");
+ 	break;
+ 	case3: //haz seleccionado mensual//
+ 	printf("sueldo es igual a /30");
+ 	break;
+    default:// Si se ha seleccionado otra cantidad //
+    printf("No se puede realizar operacion /n");
+ 
+ 
+ 
+}
+ return 0;
+
