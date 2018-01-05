a2bc436b62259d46642b819c76b5947da16b4c2d
subzero47 <35123892+subzero47@users.noreply.github.com>
2018-01-04 22:54:07 -0600

    Add files via upload

---
+++ problema de clase DEU.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ /* run this program using the console pauser on add your own getch,sysrem ("pause") or input loop*/
+int main (int argc, char**argv)
+{
+string nombre="",grupo="",licenciatura="",fecha="";
+cout<<"escriba su nombre"<<endl;
+cin>>nombre;
+cout<<"escriba el grupo en el que esta"<<endl;
+cin>>grupo;
+cout<<"escriba cual es su  licenciatura"<<endl;
+cin>>licenciatura;
+cout<<"ingrese la fecha"<<endl;
+cin>>fecha;
+cout <<"n\bienvenido al examen"<<examen<<"n\Tu nombre es"<<nombre<<"n\eres de la licenciatura de "<<licenciatura<<
+"n\eres del grupo"<<grupo<<"n\la fecha es"<<fecha<<endl;
+
+
+return 0;
+}
